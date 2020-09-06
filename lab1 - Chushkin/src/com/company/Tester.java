5fd7179871a6bf9b0267e35424066e7898e71b28
Chushkin Oleg <chushckin.ol@yandex.ru>
2020-09-06 19:19:37 +0300

    Practice 1

--- lab1 - Chushkin/src/com/company/Tester.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Tester {
-    public static void printInfo(Object d1, Object d2, Object d3){
-        String s = d1.toString() + "\n" + d2.toString() + "\n" + d3.toString();
-        System.out.println(s);
-    }
-}
