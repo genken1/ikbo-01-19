5dd6eb0b2de4649e5579dba12dace33103cf5dc8
Chushkin Oleg <chushckin.ol@yandex.ru>
2020-09-07 11:28:47 +0300

    Lab1

---
+++ Lab1 - Chushkin/src/com/company/Variant3.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Variant3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println((float) 1 / i);
+        }
+    }
+}
