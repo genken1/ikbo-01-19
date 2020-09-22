6acd02231a7e601b33f5f97ac9f2f7b3951bb7d1
Oleg Chushkin <chushckin.ol@yandex.ru>
2020-09-23 00:04:30 +0300

    fixed bugs

--- Practice6 - Chushkin/src/com/company/Exercise2/Main.java
+++ Practice6 - Chushkin/src/com/company/Exercise2/Main.java
@@ -2,6 +2,8 @@ package com.company.Exercise2;
 
 import com.company.Student;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +11,12 @@ public class Main {
         for(int i=0; i<st.length; i++){
             st[i] = new Student((int)(Math.random()*100));
         }
+        System.out.println("Sorting by QuickSort:");
         QuickSort.sort(st, 0, st.length-1);
+        for(Student e : st) System.out.println(e);
+
+        Arrays.sort(st, new SortingStudentsByGPA());
+        System.out.println("\nSorting by gpa:");
         for(Student e : st) System.out.println(e);
     }
 }
