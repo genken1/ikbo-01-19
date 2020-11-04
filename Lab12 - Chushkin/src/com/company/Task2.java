5d479ba6871edf4bda5b516ee28b5b5e8238d59f
Oleg Chushkin <chushckin.ol@yandex.ru>
2020-11-04 21:28:14 +0300

    Lab №12

---
+++ Lab12 - Chushkin/src/com/company/Task2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\d\\s*\\+");
+        String[] tests = {
+                "(1 + 8) – 9 / 4",
+                "6 / 5 – 2 * 9 "
+        };
+        for (String test : tests)
+            System.out.println(pattern.matcher(test).find() + ":\t" + test);
+    }
+}
